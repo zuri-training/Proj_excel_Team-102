b2484117c16493cddbdf080384134d389705d577
AlleyER16 <rehobothmicahdaniels@gmail.com>
2022-07-30 17:20:46 +0100

    Added diff checker operation

--- app/index.py
+++ app/index.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, UploadFile, Form
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-import aiofiles
-from datetime import datetime
-
-from app.routers import users, auth
+from app.routers import users, auth, operations
 
 app = FastAPI()
 
@@ -15,21 +12,8 @@ def root():
         "team_members": ["Rehoboth Micah-Daniels"]
     }
 
-# test endpoint
-@app.post("/api/v1/uploadfile/")
-async def upload_file(upload_file: UploadFile, description: str = Form(), user: int = Form()):
-
-    unique_prefix = abs(hash(datetime.now()))
-
-    out_file_path = f"static/uploads/{unique_prefix}_{upload_file.filename}"
-
-    async with aiofiles.open(out_file_path, 'wb') as out_file:
-        while content := await upload_file.read(1024):  # async read chunk
-            await out_file.write(content)  # async write chunk
-
-    return {"message": "Uploaded successfully", "description": description, "user": user}
-
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(operations.router)
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
---
+++ app/routers/operations.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, UploadFile, Form
+
+import aiofiles
+from datetime import datetime
+
+import os
+
+from app.operations.diff_checker import diff_checker
+
+router = APIRouter(
+    prefix="/api/v1/operations",
+    tags=["Operations"]
+)
+
+@router.post("/diff_checker/")
+async def upload_file(file1: UploadFile, file2: UploadFile):
+
+    unique_dirname = abs(hash(datetime.now()))
+    unique_dirname = f"static/files/{unique_dirname}"
+
+    os.mkdir(unique_dirname)
+
+    file1_unique_prefix = abs(hash(datetime.now()))
+
+    file1_path = f"{unique_dirname}/{file1_unique_prefix}_{file1.filename}"
+
+    async with aiofiles.open(file1_path, 'wb') as out_file:
+        while content := await file1.read(1024):  
+            await out_file.write(content)  
+
+    file2_unique_prefix = abs(hash(datetime.now()))
+
+    file2_path = f"{unique_dirname}/{file2_unique_prefix}_{file1.filename}"
+
+    async with aiofiles.open(file2_path, 'wb') as out_file:
+        while content := await file2.read(1024):  
+            await out_file.write(content)  
+
+    checked1, checked2 = diff_checker(unique_dirname, file1_path, file2_path)
+
+    return {
+        "message": "Uploaded successfully", 
+        "checked_file_1": checked1.replace("static/", ""), 
+        "checked_file_2": checked2.replace("static/", "")
+    }
