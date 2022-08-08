6bc447688ef8257e689fc42f357849b30676439b
AlleyER16 <rehobothmicahdaniels@gmail.com>
2022-08-08 15:53:40 +0100

    Made landing page responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,48 +2,67 @@ import logo from "../assets/img/logo.png";
 
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+
 const Header = () => {
+    const [modal_open, set_modal_open] = useState(false);
+
     return (
-        <header className="header">
+        <header className={`header ${modal_open && "navbar-open"}`}>
             <div className="header-content">
                 <div className="brand">
                     <img className="logo" src={logo} alt="Company Logo" />
                 </div>
-                <ul className="navbar">
-                    <li>
-                        <Link to="/" className="navbar-link">
-                            Home
+                <nav className="navbar">
+                    <ul className="navbar-nav">
+                        <li>
+                            <Link to="/" className="navbar-link">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" className="navbar-link">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/product" className="navbar-link">
+                                Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/features" className="navbar-link">
+                                Features
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact_us" className="navbar-link">
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="navbar-right">
+                        <Link to="/login" className="login">
+                            Login
                         </Link>
-                    </li>
-                    <li>
-                        <Link to="/about" className="navbar-link">
-                            About
+                        <Link to="/register" className="signup">
+                            Sign Up
                         </Link>
-                    </li>
-                    <li>
-                        <Link to="/product" className="navbar-link">
-                            Product
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/features" className="navbar-link">
-                            Features
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/contact_us" className="navbar-link">
-                            Contact Us
-                        </Link>
-                    </li>
-                </ul>
-                <div className="header-right">
-                    <Link to="/login" className="login">
-                        Login
-                    </Link>
-                    <Link to="/register" className="signup">
-                        Sign Up
-                    </Link>
-                </div>
+                    </div>
+                </nav>
+                <button
+                    className="navbar-btn"
+                    onClick={() => set_modal_open(!modal_open)}
+                >
+                    <ion-icon
+                        name="menu-outline"
+                        class="navbar-btn-icon"
+                    ></ion-icon>
+                    <ion-icon
+                        name="close-outline"
+                        class="navbar-btn-icon"
+                    ></ion-icon>
+                </button>
             </div>
         </header>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import hero_img from "../assets/img/hero-img.png";
+import features_img from "../assets/img/features-img.png";
 import checkicon from "../assets/img/checkicon.png";
 
 import Header from "../components/Header";
@@ -32,7 +33,7 @@ const Home = () => {
                 </div>
             </section>
             <section className="features-section">
-                <div className="container">
+                <div className="container features-container">
                     <div className="features-header">
                         <h3 className="features-title">Our Features</h3>
                         <p className="features-subtitle">
@@ -69,106 +70,13 @@ const Home = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div className="features-image-box"></div>
-                    </div>
-                </div>
-            </section>
-            <section className="features2-section">
-                <div className="container features2-container">
-                    <div className="feature">
-                        <div className="feature-icon-box">
-                            <ion-icon
-                                name="documents-outline"
-                                class="feature-icon"
-                            ></ion-icon>
+                        <div className="features-img-box">
+                            <img
+                                src={features_img}
+                                alt=""
+                                className="features-img"
+                            />
                         </div>
-                        <p className="feature-title">Search and Highlight</p>
-                        <p className="feature-text">
-                            our system is going to look through the uploaded
-                            excel file and check every cell any cell that
-                            matches the searched keyword would be highlighted.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-box">
-                            <ion-icon
-                                name="documents-outline"
-                                class="feature-icon"
-                            ></ion-icon>
-                        </div>
-                        <p className="feature-title">Search and Replace</p>
-                        <p className="feature-text">
-                            our system is going to look through the uploaded
-                            excel file and check every cell any cell that
-                            matches the searched keyword would be replaced with
-                            the given replace value.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-box">
-                            <ion-icon
-                                name="documents-outline"
-                                class="feature-icon"
-                            ></ion-icon>
-                        </div>
-                        <p className="feature-title">Highlight Duplicates</p>
-                        <p className="feature-text">
-                            Our system is going to look through the uploaded
-                            excel file and check every row that contains a
-                            duplicate value for the specified unique columns and
-                            highlight them.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-box">
-                            <ion-icon
-                                name="documents-outline"
-                                class="feature-icon"
-                            ></ion-icon>
-                        </div>
-                        <p className="feature-title">Remove Duplicates</p>
-                        <p className="feature-text">
-                            Our system is going to look through the uploaded
-                            excel file and check every row that contains a
-                            duplicate value for the specified unique columns and
-                            remove them.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-box">
-                            <ion-icon
-                                name="documents-outline"
-                                class="feature-icon"
-                            ></ion-icon>
-                        </div>
-                        <p className="feature-title">
-                            Highlight Duplicates and Return 2 Files
-                        </p>
-                        <p className="feature-text">
-                            Our system is going to look through the uploaded
-                            excel file and check every row that contains a
-                            duplicate value for the specified unique columns and
-                            highlight them and also generate an analysis of
-                            duplicated data.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-box">
-                            <ion-icon
-                                name="documents-outline"
-                                class="feature-icon"
-                            ></ion-icon>
-                        </div>
-                        <p className="feature-title">
-                            Remove Duplicates and Return 2 Files
-                        </p>
-                        <p className="feature-text">
-                            Our system is going to look through the uploaded
-                            excel file and check every row that contains a
-                            duplicate value for the specified unique columns and
-                            remove them and also return an excel sheet
-                            containing the removed rows.
-                        </p>
                     </div>
                 </div>
             </section>
@@ -179,7 +87,7 @@ const Home = () => {
                 </div>
             </section>
             <section className="testimonial-section">
-                <div className="testimonial-container">
+                <div className="container testimonial-container">
                     <ion-icon
                         name="chevron-back-circle-outline"
                         class="testimonial-icon testimonial-icon--left"
@@ -210,7 +118,7 @@ const Home = () => {
                 </div>
             </section>
             <section className="newsletter-section">
-                <div className="newsletter-container">
+                <div className="container">
                     <h3 className="newsletter-title">
                         Subscribe for our updates
                     </h3>
